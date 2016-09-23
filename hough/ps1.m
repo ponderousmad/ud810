5831dab1784e25ffe40d8f2ce6ed8b5d22374f19
Adrian Smith <agnomen@gmail.com>
2016-09-22 23:11:57 -0400

    Mark peaks.

--- hough/ps1.m
+++ hough/ps1.m
@@ -20,6 +20,12 @@ colormap(gca,hot);
 %% 2-b
 peaks = hough_peaks(H, 10);  % defined in hough_peaks.m
 %% TODO: Highlight peak locations on accumulator array, save as output/ps1-2-b-1.png
+
+color_H = repmat(img_h,[1,1,3]);
 disp(peaks);
 
+for p = peaks'
+    color_H(p(1), p(2), 1:2) = 0;
+end
+imwrite(color_H, fullfile('output', 'ps1-2-b-1.png'));
 %% TODO: Rest of your code here
